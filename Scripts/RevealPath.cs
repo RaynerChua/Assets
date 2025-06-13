44963c91de57d9faa21fd555e4a7aaf506c3394d
Chua Yi Xuan Rayner <S10270587@connect.np.edu.sg>
2025-06-13 18:12:09 +0800

    slight edit to reveal path
    had to move the destroy game object function as it was preventing the message pop up from hiding

--- Scripts/RevealPath.cs
+++ Scripts/RevealPath.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class RevealPathToken : MonoBehaviour
 {
     [SerializeField] GameObject hiddenPath;
+    [SerializeField] TextMeshProUGUI revealMsgText;
 
     public void Collect()
     {
@@ -11,7 +14,22 @@ public class RevealPathToken : MonoBehaviour
             hiddenPath.SetActive(true); // Reveal the path
             Debug.Log("Hidden path revealed!");
         }
+        if (revealMsgText != null)
+        {
+            revealMsgText.gameObject.SetActive(true); // Show the message UI
+            StartCoroutine(HideMsg());
+        }
 
+    }
+
+    public IEnumerator HideMsg()
+    {
+        yield return new WaitForSeconds(3f); // Wait for 5 seconds
+        if (revealMsgText != null)
+        {
+            revealMsgText.gameObject.SetActive(false); // Hide the message UI
+        }
+        Debug.Log("Message hidden after 2 seconds.");
         Destroy(gameObject); // Remove the token after use
     }
 }
