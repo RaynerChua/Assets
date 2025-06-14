b52009f8a081fc1dea3f0c1e0f9e192eeb531bc0
Chua Yi Xuan Rayner <S10270587@connect.np.edu.sg>
2025-06-14 17:01:47 +0800

    DoorBehaviour audio
    audio code edit

--- Scripts/DoorBehaviour.cs
+++ Scripts/DoorBehaviour.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DoorBehaviour : MonoBehaviour
 {
+    AudioSource doorAudioSource;
+    private bool isOpen = false;
+
+    void Start()
+    {
+        doorAudioSource = GetComponent<AudioSource>();
+        if (doorAudioSource == null)
+        {
+            Debug.LogError("AudioSource component is missing on the door object.");
+        }
+    }
+
     public void Interact()
     {
         Vector3 doorRotation = transform.eulerAngles;
@@ -9,12 +22,19 @@ public class DoorBehaviour : MonoBehaviour
         {
             doorRotation.y += 90f;
             transform.eulerAngles = doorRotation;
+            isOpen = true;
         }
         else if (doorRotation.y == 90f)
         {
             doorRotation.y = 0f;
             transform.eulerAngles = doorRotation;
+            isOpen = false;
+        }
+
+        // Only play sound when door is opening
+        if (isOpen && doorAudioSource != null)
+        {
+            doorAudioSource.Play();
         }
     }
-
 }
